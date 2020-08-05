baee93a42125275752ce66bf8ef02317a6218150
Théo Vielmas <theo.vielmas@gmail.com>
2020-08-05 15:45:01 +0200

    Disable menu in sign-in page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import {MenuController, Platform} from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -40,9 +40,9 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
+    const path = window.location.pathname;
     if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+      this.selectedIndex = this.appPages.findIndex(page => page.url.toLowerCase() === path.toLowerCase());
     }
   }
 }
--- src/app/pages/sign-in/sign-in.page.ts
+++ src/app/pages/sign-in/sign-in.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MenuController} from "@ionic/angular";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInPage implements OnInit {
 
-  constructor() { }
+  constructor(
+      private menu: MenuController,
+      private router: Router
+  ) { }
 
   ngOnInit() {
+    this.disableMenu();
   }
 
+  async disableMenu() {
+    await this.menu.enable(false);
+  }
+
+  async signIn() {
+    await this.menu.enable(true)
+    await this.router.navigate(['/top-anime'])
+  }
 }
