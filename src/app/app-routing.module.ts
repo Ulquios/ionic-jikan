7496c793e3e663467dc45567b847587c53a2e1e9
Ulquios <theo.vielmas@gmail.com>
2020-08-19 18:41:01 +0200

    Fix router mistake

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'top-anime',
     loadChildren: () => import('./pages/top-anime/top-anime.module').then( m => m.TopAnimePageModule)
+  },
+  {
+    path: 'top-anime-stencil',
+    loadChildren: () => import('./pages/top-anime-stencil/top-anime-stencil.module').then( m => m.TopAnimeStencilPageModule)
   }
 ];
 
